7eaf842ff6208caa10f3288614880e143d415ee2
Ram <heath.raman@gmail.com>
2021-10-24 19:38:11 +0100

    added basic singleton

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='patterns',
+    version='0.1.0-pre-release',
+    description='Some basic design pattern helpers in python',
+    scripts=[],
+    url='',
+    author='Ram',
+    author_email='heath.raman@gmail.com',
+    license='MIT',
+    install_requires=[],
+    packages=['patterns'
+              ],
+    package_data={},
+    zip_safe=False
+)
